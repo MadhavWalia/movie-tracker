a6fc4fb7164d39ab0d53e13b825af4372fa555b6
Madhav_Walia <madhavwalia8139@gmail.com>
2023-06-14 22:47:18 +0530

    Added the CORS and prometheus middleware

--- api/api.py
+++ api/api.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
+from starlette.middleware.cors import CORSMiddleware
 from api.handlers import demo, movie_v1
 
 
 def create_app():
     app = FastAPI(docs_url="/")
+
+    #Middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    Instrumentator().instrument(app).expose(app)
 
     # Routers
     # app.include_router(demo.router)
